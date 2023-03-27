94936d2a21f50d437ec1f7b6f6f0373f630f07d2
Xuanhai244 <Thebreaker015@gmail.com>
2023-03-27 19:02:15 +0700

    commit chuc nang dang ky dang nhap

--- apps/controllers/homeController.js
+++ apps/controllers/homeController.js
@@ -6,8 +6,6 @@ var DataService = require("./../services/dataService");
 router.get("/", async function(req,res){
     var dataService = new DataService();
     req.session.categories = await dataService.getCategoryList();
-    //req.session.discountFoods = await dataService.getDiscountFoodList();
-    //var discountFoods = req.session.discountFoods;
     req.session.products = await dataService.getProductList()
     var products=req.session.products;
     var categories =  req.session.categories;
